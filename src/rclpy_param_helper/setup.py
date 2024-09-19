a7384aba1f1224e1dfc48ba50e37694c0d925452
FrankSav80 <francesco.savino80@gmail.com>
2024-09-19 09:36:59 +0200

    Update setup.py
    Adattamento a ros1

--- src/rclpy_param_helper/setup.py
+++ src/rclpy_param_helper/setup.py
@@ -7,15 +7,13 @@ setup(
     version='0.0.1',
     packages=find_packages(),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools', 'numpy', 'rclpy', 'ros2param'],
+    install_requires=['setuptools', 'numpy', 'rospy'],
     zip_safe=True,
     maintainer='Ricardo de Azambuja',
     maintainer_email='ricardo.azambuja@gmail.com',
-    description='Convert between Python dictionary and ROS2 parameters',
+    description='Convert between Python dictionary and ROS parameters for ROS 1',
     license='MIT',
     tests_require=['pytest'],
 )
