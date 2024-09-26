3865e34037c8c843a4efd8597c5003f691af14f0
FrankSav80 <francesco.savino80@gmail.com>
2024-09-26 17:51:33 +0200

    Update setup.py

--- src/ros2_quad_sim_python/setup.py
+++ src/ros2_quad_sim_python/setup.py
@@ -23,12 +23,5 @@ setup(
     maintainer_email='ricardo.azambuja@gmail.com',
     description='Quadcopter simulator and controller based on Python',
     license='MIT',
-    tests_require=['pytest'],
-    entry_points={
-        'console_scripts': [
-            'quadsim = ros2_quad_sim_python.ros_quad_sim:main',
-            'quadctrl = ros2_quad_sim_python.ros_quad_ctrl:main',
-            'quad = ros2_quad_sim_python.ros_quad_sim_and_ctrl:main'
-        ],
-    },
+    tests_require=['pytest']
 )
